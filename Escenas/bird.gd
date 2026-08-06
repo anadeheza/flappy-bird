2176db24130017c4961a63441030a4aa5eefb53a
Ana <adehezaflaherty@gmail.com>
2026-08-05 21:17:20 -0300

    kept going with mechanics, changed bird to superman

--- Escenas/bird.gd
+++ Escenas/bird.gd
@@ -1,15 +1,36 @@
 extends CharacterBody2D
 
+signal died 
 
 const GRAVITY = 900.0
 const JUMP_FORCE = -350.0
 
+var vive = false 
+var can_move = false 
 
 func _physics_process(delta):
+	if not can_move:
+		return 
+	
 	velocity.y += GRAVITY * delta
 
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and vive:
 		velocity.y = JUMP_FORCE
 	move_and_slide()
 	
-	rotation_degrees = clamp(velocity.y / 10.0, -30, 90)
+	if vive and get_slide_collision_count() > 0:
+		die()
+		
+func start(): 
+	vive = true 
+	can_move = true
+	velocity = Vector2.ZERO 
+	rotation_degrees = 0
+	
+func die():
+	if not vive:
+		return
+	
+	vive = false
+	can_move = false
+	died.emit()
--- Escenas/main.gd
+++ Escenas/main.gd
@@ -3,17 +3,89 @@ extends Node2D
 const PIPE_SCENE = preload("res://Escenas/pipe.tscn")
 const PIPE_SPAWN_X = 600.0 
  
+enum GameState {
+	MENU,
+	PLAYING,
+	GAMEOVER
+}
+
+var game_state = GameState.MENU 
 var score = 0
 
+@onready var bird = $Bird 
+@onready var pipe_timer = $PipeSpawnTimer
+
 func _ready():
-	$PipeSpawnTimer.timeout.connect(_on_pipe_spawn_timer_timeout)
+	pipe_timer.timeout.connect(_on_pipe_spawn_timer_timeout)
+	bird.died.connect(_on_bird_died)
+	
+	pipe_timer.stop()
+	
+	_show_menu()
+	
+func _input(event):
+	if not event.is_action_pressed("jump"):
+		return
+	
+	if game_state == GameState.MENU:
+		start_game()
+	elif game_state == GameState.GAMEOVER:
+		restart_game()
+		
+
+func start_game():
+	game_state = GameState.PLAYING
+	
+	score = 0
+	
+	$UI/Menu.visible = false 
+	$UI/GameOver.visible = false 
+	$UI/Score.visible = true
+	
+	bird.position = Vector2(93, 442)
+	bird.start()
+	pipe_timer.start()
 
 func _on_pipe_spawn_timer_timeout():
+	if game_state != GameState.PLAYING:
+		return
+	
 	var pipe = PIPE_SCENE.instantiate()
-	pipe.position = Vector2(PIPE_SPAWN_X, randi_range(200, 400))
+	
+	pipe.position = Vector2(
+		PIPE_SPAWN_X, 
+		randi_range(200, 400)
+	)
+	
 	add_child(pipe)
 	pipe.scored.connect(_on_pipe_scored)
 
 func _on_pipe_scored():
+	if game_state != GameState.PLAYING:
+		return
+	
 	score += 1
-	print(score)
+	$UI/Score/ScoreLabel.text = str(score)
+
+func _on_bird_died():
+	if game_state != GameState.PLAYING:
+		return
+	
+	game_state = GameState.GAMEOVER
+	
+	pipe_timer.stop()
+	$UI/GameOver.visible = true
+
+func restart_game():
+	for child in get_children():
+		if child.scene_file_path == "res://Escenas/pipe.tscn": 
+			child.queue_free()
+	
+	start_game()
+
+func _show_menu():
+	game_state = GameState.MENU 
+	
+	$UI/Menu.visible = true 
+	$UI/GameOver.visible = false 
+	$UI/Score.visible = false
